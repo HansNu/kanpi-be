e2461c659ec55f3bdd9cdfc6d9684bcce419775d
HansN <araphura@gmail.com>
2025-02-09 20:26:28 +0700

    tes

--- userRoutes.js
+++
@@ -1,9 +0,0 @@
-// userRoutes.js
-const express = require('express');
-const userController = require('../controllers/userController');  // Change this
-const router = express.Router();
-const baseUserUrl = '/user';
-
-router.post(baseUserUrl + '/addUser', userController.AddUser);  // Reference controller
-
-module.exports = router;

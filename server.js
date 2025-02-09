d153ad560fcd1e2d484f7e370a513d4be96e5b01
HansN <araphura@gmail.com>
2025-02-09 20:01:25 +0700

    server settings

---
+++ server.js
@@ -0,0 +1,35 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swagger');
+const userRoutes = require('./userRoutes');
+const supabase = require('../services/supabaseClient');
+
+// Load environment variables
+dotenv.config();
+
+console.log('Supabase URL:', process.env.SUPABASE_URL);
+console.log('Supabase Key:', process.env.SUPABASE_KEY);
+
+// Initialize Express app
+const app = express();
+app.use(express.json());
+
+const port = 4200;
+const baseUrl = '/api';
+
+// Base route
+app.get('/', (req, res) => {
+    res.send('This is root node');
+});
+
+// Swagger documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// User routes
+app.use(baseUrl, userRoutes); // Example: /api/user
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
